c31c9701064a80d9c9fae8fb2b46557392c8fe49
yousef-ghanim <yousefghanim85@gmail.com>
2021-05-23 23:01:45 +0000

    first commit

---
+++ src/components/Repos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Repos = () => {
+  return <h1>repos</h1>;
+};
+
+export default Repos;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import loadingImage from "../images/preloader.gif";
+import Navbar from "../components/Navbar";
+import Info from "../components/Info";
+import Search from "../components/Search";
+import Repos from "../components/Repos";
+import User from "../components/User";
+const Dashboard = () => {
+  return (
+    <main>
+      <Navbar />
+      <Search />
+      <Info />
+      <User />
+      <Repos />
+    </main>
+  );
+};
+
+export default Dashboard;
